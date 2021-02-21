65d1308a4add178850569ad16bd972d8c350943d
Ilya Makarov <feesler@gmail.com>
2021-02-21 05:06:51 +0300

    * Fix two digits year issue: allow to input date both in DD.MM.YYYY and DD.MM.YY formats - Assume for two digits year it is 2000 and later, not 1900-1999

--- src/models/StepsRecord.js
+++ src/models/StepsRecord.js
@@ -1,17 +1,33 @@
-/* Parse date string in format DD.MM.YYYY and return timestamp or NaN if value is incorrect */
+/* Parse date string in DD.MM.YY or DD.MM.YYYY format and return timestamp or NaN if value is incorrect */
 function parseDate(value) {
   if (typeof value !== 'string') {
     return NaN;
   }
 
-  const matches = value.match(/^(\d{2})\.(\d{2}).(\d{4})$/);
+  const matches = value.match(/^(\d{2})\.(\d{2}).(\d{2}|\d{4})$/);
   if (!matches) {
     return NaN;
   }
 
-  const [, ...parts] = matches;
+  let [, day, month, year] = matches;
+  day = parseInt(day, 10);
+  month = parseInt(month, 10) - 1;
+  year = parseInt(year, 10);
+  // Assume for two digits year its 2000 and later, not 1900-1999
+  if (year < 100) {
+    year += 2000;
+  }
 
-  return Date.parse(parts.reverse().join('-'));
+  const date = new Date(year, month, day);
+  if (
+    date.getDate() !== day
+    || date.getMonth() !== month
+    || date.getFullYear() !== year
+  ) {
+    return NaN;
+  }
+
+  return date.getTime();
 }
 
 /* Parse decimal distance value and return result or NaN if value is incorrect */
